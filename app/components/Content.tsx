74791df843a0604f5d6a0c7ebb6648ff2748b396
SebaRossi94 <seba.rossi.94@gmail.com>
2025-02-09 02:32:01 -0300

    updated main Content component to support recursive content with list

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { useTranslation } from "react-i18next"
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import {
   Typography,
   Container,
@@ -14,35 +14,94 @@ import {
   DialogContent,
   DialogActions,
   Button,
-} from "@mui/material"
-import Grid from '@mui/material/Grid2';
-import { motion, AnimatePresence } from "framer-motion"
-import { useWindowSize } from "../hooks/useWindowSize"
+  List,
+  ListItem,
+} from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { motion, AnimatePresence } from "framer-motion";
+import { useWindowSize } from "../hooks/useWindowSize";
 
-export default function Content({ title, cardContentsKey }: { title: string, cardContentsKey: string}) {
-  const { t } = useTranslation()
-  const [selectedCard, setSelectedCard] = useState<number | null>(null)
-  const { width } = useWindowSize()
+export default function Content({
+  title,
+  cardContentsKey,
+}: {
+  title: string;
+  cardContentsKey: string;
+}) {
+  const { t } = useTranslation();
+  const [selectedCard, setSelectedCard] = useState<number | null>(null);
+  const { width } = useWindowSize();
 
-  const isMobile = width !== undefined && width <= 768
+  const isMobile = width !== undefined && width <= 768;
 
-  const cardContents = t(`${cardContentsKey}.list`, { returnObjects: true }) as Array<{ title: string; description: string, close: string }>
+  const cardContents = t(`${cardContentsKey}.list`, {
+    returnObjects: true,
+  }) as Array<{
+    title: string;
+    subtitle?: string;
+    description: string | Array<string>;
+    close: string;
+  }>;
+
+  const renderDescription = (description: any) => {
+    if (Array.isArray(description)) {
+      return (
+        <List>
+          {description.map((item, index) => (
+            <ListItem key={index}>{renderDescription(item)}</ListItem>
+          ))}
+        </List>
+      );
+    }
+    if (typeof description === "object" && description !== null) {
+      return (
+        <>
+          {Object.entries(description).map(([key, value], index) => (
+            <>
+              <Typography key={key} variant="h6">
+                {key}
+              </Typography>
+              <List key={key}>
+                <ListItem key={index}>{renderDescription(value)}</ListItem>
+              </List>
+            </>
+          ))}
+        </>
+      );
+    }
+    return <Typography>{description}</Typography>;
+  };
 
   return (
     <Container maxWidth="md" sx={{ py: 4 }}>
       <Box sx={{ textAlign: isMobile ? "center" : "left" }}>
-        <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
-          <Typography variant={isMobile ? "h3" : "h2"} component="h1" gutterBottom>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Typography
+            variant={isMobile ? "h3" : "h2"}
+            component="h1"
+            gutterBottom
+          >
             {t(title)}
           </Typography>
           <Grid container spacing={3}>
-            {cardContents.map((course, index) => (
-              <Grid size={{xs: 12, sm: 6,md: 4}}  key={index}>
+            {cardContents.map((card, index) => (
+              <Grid size={{ xs: 12, sm: 6, md: 4 }} key={index}>
                 <Card>
                   <CardActionArea onClick={() => setSelectedCard(index)}>
                     <CardContent>
                       <Typography variant="h6" component="div">
-                        {course.title}
+                        {card.title}
+                      </Typography>
+                      <Typography
+                        variant="subtitle2"
+                        color="secondary"
+                        component="div"
+                      >
+                        {card.subtitle}
                       </Typography>
                     </CardContent>
                   </CardActionArea>
@@ -54,18 +113,24 @@ export default function Content({ title, cardContentsKey }: { title: string, car
       </Box>
       <AnimatePresence>
         {selectedCard !== null && (
-          <Dialog open={selectedCard !== null} onClose={() => setSelectedCard(null)} fullWidth maxWidth="sm">
+          <Dialog
+            open={selectedCard !== null}
+            onClose={() => setSelectedCard(null)}
+            fullWidth
+            maxWidth="sm"
+          >
             <DialogTitle>{cardContents[selectedCard].title}</DialogTitle>
             <DialogContent>
-              <Typography>{cardContents[selectedCard].description}</Typography>
+              {renderDescription(cardContents[selectedCard].description)}
             </DialogContent>
             <DialogActions>
-              <Button onClick={() => setSelectedCard(null)}>{t(`${cardContentsKey}.close`)}</Button>
+              <Button color="secondary" onClick={() => setSelectedCard(null)}>
+                {t(`${cardContentsKey}.close`)}
+              </Button>
             </DialogActions>
           </Dialog>
         )}
       </AnimatePresence>
     </Container>
-  )
+  );
 }
-
